ce4cd389d322c2fcf902a9e5a414ac8bdc1d8bf2
Charles Jennings <Charlesjenn39@gmail.com>
2017-10-31 13:14:26 -0500

    Basic Start

---
+++ Grade Calculator/Calculator_board.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  Grade Calculator
+//
+//  Created by Charles Jennings on 7/13/17.
+//  Copyright © 2017 Charles Jennings. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Subject :  Grade
+{
+    
+}
+class Grade
+{
+    var percentageOfTotalGrade
+}
+
+class Calculator :  UIViewController
+{
+    
+    var math = Subject()
+    
+    
+}
